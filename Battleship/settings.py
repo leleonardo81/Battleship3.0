201d8c354d85c4835cb801da859f5cbcfc73978e
leonardo81 <leonardoeinstein2000@gmail.com>
2019-04-22 21:52:02 +0700

    deploy settings 1

--- Battleship/settings.py
+++ Battleship/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'channels',
     'playerAuth',
     'playground',
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'Battleship.urls'
