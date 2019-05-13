fdc9dc5b921cbae4096d86fa0eb4249e9ed817e7
leonardo81 <leonardoeinstein2000@gmail.com>
2019-05-13 18:55:07 +0700

    try this one

--- Battleship/settings.py
+++ Battleship/settings.py
@@ -138,5 +138,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
 )
-import django_heroku
-django_heroku.settings(locals())
+# import django_heroku
+# django_heroku.settings(locals())
